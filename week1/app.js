fa0021c59b57907f068922c53e8e323d976b9429
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-08-02 23:01:12 -0700

    Node Math Assignment

---
+++ week1/app.js
@@ -0,0 +1,15 @@
+const sum = require("./math.js")
+
+const { multiply, setFactor } = require('./math2.js')
+
+const User = require('./user.js')
+const { import } = require("mathjs")
+// console.log(sum(20, 10))
+
+// console.log(User)
+
+console.log(new User("steve", 10))
+
+import
+export
+export default
---
+++ week1/math3.js
@@ -0,0 +1,11 @@
+let factor = 2
+
+function devide(num){
+    return num * factor
+}
+
+function setFactor(newFactor){
+    factor = newFactor
+}
+
+module.export = { devide, setFactor }
